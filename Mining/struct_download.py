a218011a6a2ff54894c09bc12516e2f8661ead45
dptru10 <dptru10@gmail.com>
2020-07-14 13:40:20 -0400

    added code for pulling data from 'not_so_good.csv'

---
+++ Mining/struct_download.py
@@ -0,0 +1,17 @@
+from pymatgen.ext.matproj import MPRester 
+from pymatgen.io.vasp.sets import MPRelaxSet, VaspInputSet
+import pandas as pd
+
+data = pd.read_csv('not_so_good.csv')
+
+mpr = MPRester() 
+
+user_incar_settings={}
+hubbard_off = False 
+
+i=0
+for mp_id in data['task_id']: 
+    struct = mpr.get_structure_by_material_id(str(mp_id))
+    relaxed    = MPRelaxSet(struct)
+    relaxed.write_input("/tmp/structs/%s_%s" %(mp_id,data['labels'].iloc[i]),make_dir_if_not_present=True, potcar_spec=True)
+    i+=1 
