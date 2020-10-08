50137395c60075707102673c567c79e27684d920
dptru10 <dptru10@gmail.com>
2020-10-07 23:35:00 -0400

    removed average on eigs

--- electrostrict_coeffs/electrostrict_coeffs_eigs.py
+++ electrostrict_coeffs/electrostrict_coeffs_eigs.py
@@ -1,13 +1,12 @@
 import numpy as np 
 import pandas as pd 
 
-eps_0 = 8.854e-12
+eps_0  = 8.854e-12
 
 data_p = pd.read_csv('Promising19_compounds_epsilion_components_Mh_plus25kb.csv')
 data_m = pd.read_csv('Promising19_compounds_epsilion_components_Mh_minus25kb.csv')
 deltaP = pd.read_csv('Promising19_compounds_dP.csv') 
 dP     = deltaP['deltaP'] 
-
 
 #eig_p
 eig_p=[]
@@ -32,17 +31,16 @@ chi_ave_p = []
 eps_ave_p = []
 for i in range(len(dP)): 
 	chi   = 1/eig_p[i]
-	chi_ave_p.append((1/3*eps_0) * (chi))
-	eps_ave_p.append((1/3*eps_0) * (eig_p[i]))#data_p['e11'].iloc[i] + data_p['e22'].iloc[i] + data_p['e33'].iloc[i])) 
+	chi_ave_p.append(eps_0 * (chi))
+	eps_ave_p.append(eps_0 * (eig_p[i]))#data_p['e11'].iloc[i] + data_p['e22'].iloc[i] + data_p['e33'].iloc[i])) 
 
 #chi_minus 
 chi_ave_m = []
 eps_ave_m = []
 for i in range(len(dP)): 
 	chi   = 1/eig_m[i]
-	chi_ave_m.append((1/3*eps_0) * (chi))
-	eps_ave_m.append((1/3*eps_0) * (eig_m[i]))#data_m['e11'].iloc[i] + data_m['e22'].iloc[i] + data_m['e33'].iloc[i])) 
-
+	chi_ave_m.append(eps_0 * (chi))
+	eps_ave_m.append(eps_0 * (eig_m[i]))#data_m['e11'].iloc[i] + data_m['e22'].iloc[i] + data_m['e33'].iloc[i])) 
 
 #Qh & Mh 
 Qh=[]
@@ -50,8 +48,7 @@ Mh=[]
 for i in range(len(dP)): 
 	Qh.append(np.abs(chi_ave_p[i] - chi_ave_m[i])/dP[i]) 
 	Mh.append(np.abs(eps_ave_p[i] - eps_ave_m[i])/dP[i])
-print(Qh)
-print(Mh)
+
 data_out = pd.DataFrame(columns=['compound','Mh','Qh'])
 data_out['compound'] = deltaP['Compounds']
 data_out['Mh'] = Mh 
